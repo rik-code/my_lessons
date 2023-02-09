1a61d44f4d74ee7a7f6abb564db5afdd8fa39fa8
Alexey Rytsku <topelven1234567890@gmail.com>
2023-02-09 19:33:15 +0300

    задача №2

--- 11_квадратные_матрицы.cpp
+++ 11_квадратные_матрицы.cpp
@@ -36,6 +36,34 @@ void task1_2(int **array, int lines) {
     }
 }
 
+void task2(int **array, int lines) {
+    long sum = 0;
+    bool neg = false;
+    bool match = false;
+    for (int i = 0; i < lines; i++) {
+        for (int j = 0; j < lines; j++) {
+            if (array[i][j] < 0) {
+                neg = true;
+                break;
+            }
+        }
+        if (!neg && !match) {
+            for (int j = 0; j < lines; j++) {
+                sum += array[i][j];
+            }
+            match = true;
+            break;
+        }
+    }
+
+    for (int i = 0; i < lines; i++) {
+        for (int j = 0; j < lines; j++) {
+            array[i][j] -= sum;
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 
 int main() {
@@ -46,5 +74,6 @@ int main() {
     int **matrix = new int *[n];
     fill_matrix(matrix, n);
     task1_2(matrix, n);
+    task2(matrix, n);
     return 0;
 }
